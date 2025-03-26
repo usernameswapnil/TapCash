4781ead63ae220460f337416759644e11bac2b4a
Swapnil Kumar Singh <124446138+usernameswapnil@users.noreply.github.com>
2025-03-26 14:45:02 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -30,6 +30,21 @@ const userSchema=mongoose.Schema({
 })
 mongoose.model("User",userSchema);
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
+
 module.exports={
-    User
+    User,
+    Account
 }
