7a18c78dfa88b82df31e3075d465cc35ad9fe74b
Swapnil Kumar Singh <124446138+usernameswapnil@users.noreply.github.com>
2025-03-28 16:08:11 +0530

    Update account.js

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,4 +1,14 @@
 const express=require("express");
 const router=express.Router();
+const { Account } = require("../db");
+const { authMiddleware } = require('../middleware');
+router.get("/balance",authMiddleware,async function(req,res){
+    const account=await Account.findOne({
+        userId:req.userId
+    })
+    res.json({
+        balance: account.balance
+    })
+})
 module.exports=router;
 
