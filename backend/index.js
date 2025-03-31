75b14b2d0e54d72399635cce7ae0d3172cfe59b8
Swapnil Kumar Singh <124446138+usernameswapnil@users.noreply.github.com>
2025-03-31 17:50:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/v1", rootRouter);
+app.listen(port, function () {
+    console.log("server is running on port 3000");
+})
 
 
